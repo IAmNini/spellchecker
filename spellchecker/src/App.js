7e0ae6e461a997674cd4a7cc8aae699cbd78f705
IAmNini <fortuny.carrega@gmail.com>
2021-11-21 23:51:01 +0000

    spellchecker works but either forgets or remembers forever

---
+++ spellchecker/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Spellchecker from './components/Spellchecker';
+import './App.css';
+
+function App() {
+  return <>
+    <Spellchecker />
+  </>;
+}
+
+export default App;
